e32624a35c263a9c523261a11f1ea48de25ec6be
Nicocarrix <nickolascarrizo@gmail.com>
2023-08-09 22:49:46 -0300

    prueba tecnica react trainee

--- proyects/04-react-prueba-tecnica/tests/example.spec.js
+++ proyects/04-react-prueba-tecnica/tests/example.spec.js
@@ -1,19 +1,18 @@
 // @ts-check
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-const CAT_PREFIC_IMAGE_URL = 'https://cataas.com'
-const LOCALHOST_URL = 'http://localhost:5173/'
+const CAT_PREFIC_IMAGE_URL = 'https://cataas.com';
+const LOCALHOST_URL = 'http://localhost:5173/';
 
 test('app shows ramdom fact and image', async ({ page }) => {
   await page.goto(LOCALHOST_URL);
 
-  const text = await page.getByRole('paragraph')
-  const image = await page.getByRole('img')
+  const text = await page.getByRole('paragraph');
+  const image = await page.getByRole('img');
 
-  const textContent = await text.textContent()
-  const imageSrc = await image.getAttribute('src')
+  const textContent = await text.textContent();
+  const imageSrc = await image.getAttribute('src');
 
-  await expect(textContent?.length).toBeGreaterThan(0)
-  await expect(imageSrc?.startsWith(CAT_PREFIC_IMAGE_URL)).toBeTruthy()
-
+  await expect(textContent?.length).toBeGreaterThan(0);
+  await expect(imageSrc?.startsWith(CAT_PREFIC_IMAGE_URL)).toBeTruthy();
 });
